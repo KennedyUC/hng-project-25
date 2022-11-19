0fbadb8c992475948f97209e53d87fe25baf3636
kennedyuc <chukwukakennedyuche@gmail.com>
2022-11-19 10:04:16 -0800

    scripts updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,8 @@ import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Example from "./pages/Example";
 import Docs from "./pages/Docs";
+import CsvUpload from "pages/CsvUpload";
+import HelpCenter from "pages/helpCenter/HelpCenter";
 import SignUp from "./pages/auth/SignUp";
 import Terms from "./pages/Terms";
 
@@ -28,6 +30,8 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/terms" element={<Terms />} />
           <Route path="/docs/*" element={<Docs />} />
+          <Route path="/csv-upload" element={<CsvUpload />} />
+          <Route path="/help" element={<HelpCenter />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
